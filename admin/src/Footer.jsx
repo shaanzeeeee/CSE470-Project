ca4fef8ad75fff8d19c4d11694ae5a81575db12c
ultrasonicgammaray <142178724+ultrasonicgammaray@users.noreply.github.com>
2024-04-04 09:55:25 +0600

    Add files via upload

---
+++ admin/src/Footer.jsx
@@ -0,0 +1,13 @@
+//foot
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <link rel="stylesheet" type="text/css" href="/css/footer/styles.css" />
+      <p>Copyright © 2024 Thikachi. All rights reserved.</p>
+    </div>
+  );
+}
+
+export default Footer;
